160fbcb9b2909b249eb1c39e160625ed9bcbc73d
VoThangVN <78263487+voquyetthang@users.noreply.github.com>
2025-10-03 21:11:38 +0700

    Update x-ui.sh

--- x-ui.sh
+++ x-ui.sh
@@ -1,6 +1,5 @@
 apt update -y
 apt upgrade -y
-cd
 bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh)
 wget https://raw.githubusercontent.com/voquyetthang/thangvoquyet46/main/huy-cai-dat.sh
 clear
@@ -14,4 +13,4 @@ neofetch
 echo -e "Kiểm tra địa chỉ ip"
 curl ipinfo.io
 echo -e "Nếu bạn muốn hủy cài đặt hãy nhập "bash huy-cai-dat.sh
-##Copyright ZingFast And HappyNetWork##
+##VoThangVN##
